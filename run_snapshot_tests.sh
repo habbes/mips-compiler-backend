2a051d47d4adaf1793a054d0fcbb4df2bd93e00d
Clément Habinshuti <hello@habbes.xyz>
2020-04-18 04:19:00 +0300

    Add snapshot tests for block allocator

--- run_snapshot_tests.sh
+++ run_snapshot_tests.sh
@@ -1,3 +1,9 @@
-./run_quickcheck.sh naive > snapshots_temp.txt
-diff snapshots-naive.txt snapshots_temp.txt
-rm snapshots_temp.txt
+
+allocators=("naive" "block")
+
+for allocator in ${allocators[@]}; do
+    echo "Testing $allocator allocator snapshots"
+    ./run_quickcheck.sh $allocator > snapshots_temp.txt
+    diff snapshots-$allocator.txt snapshots_temp.txt
+    rm snapshots_temp.txt
+done
